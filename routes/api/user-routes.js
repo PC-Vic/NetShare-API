a3ae67314767164f6db1be764dace8848e7dec59
Victor Nieves <c_cc_vic@yahoo.com>
2023-10-27 22:33:50 -0400

    added all user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const { User } = require('../../models'); 
+
+// Route to get all users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Route to create a new user
+router.post('/users', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+// Route to get a single user by their ID
+router.get('/users/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Route to update a user by their ID
+router.put('/users/:userId', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+// Route to delete a user by their ID
+router.delete('/users/:userId', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(deletedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
