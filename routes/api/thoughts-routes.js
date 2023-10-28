14d81d04fa30b565667ca0fda8293ddfb6f37216
Victor Nieves <c_cc_vic@yahoo.com>
2023-10-27 21:53:19 -0400

    added get rout to thoughts file

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Thought } = require('../../models');
 
+// Route to get all thoughts
 router.get('/thoughts', async (req, res) => {
     try{
         const thoughts = await Thought.find();
@@ -8,5 +9,29 @@ router.get('/thoughts', async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
+    }
+});
+
+// route to create a new thought
+router.post('/thoughts', async (req, res) => {
+    try{
+        const newThought = await Thought.create(req.body);
+        res.status(201).json(newThought);
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+});
+
+// route to get a single thought by ID
+router.get('/thoughts/:thoughtId', async (req, res) => {
+    try{
+        const thought = await Thought.findById(req.params.thoughtId);
+        if (!thought) {
+            return res.status(404).json({ message: 'Thought not found' });
+        }
+        res.json(thought);
+    } catch (err) {
+        console.error(500).json(err);
     }
 });
